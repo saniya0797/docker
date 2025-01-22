34f6cc7ab2030151bfe24c61516a26633a20ade9
saniya0797 <69213333+saniya0797@users.noreply.github.com>
2025-01-22 00:23:45 -0800

    Cd2

--- my-project/src/main/java/com/example/App.java
+++ my-project/src/main/java/com/example/App.java
@@ -20,6 +20,7 @@ public class App {
     {
         Logger log = LoggerFactory.getLogger(App.class);
         log.info("Java app started");
+        log.info("hello");
     }
 
     public String getStatus() {
